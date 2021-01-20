00a59220ec37923ca7913d55773bd5ff98ace478
Ohranj <ajdorrington@hotmail.co.uk>
2021-01-20 20:38:43 +0000

    initial commit

--- model.py
+++ model.py
@@ -1,20 +1,20 @@
-import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow as tf
-import tensorflow_datasets as tfds
-import pathlib
+
+from tensorflow import keras
 from tensorflow.keras import layers
-from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPool2D, Dropout
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.callbacks import EarlyStopping
+
 
 dataset_url = "https://storage.googleapis.com/download.tensorflow.org/example_images/flower_photos.tgz"
-data_dir = tf.keras.utils.get_file(origin=dataset_url, 
-                                   fname='flower_photos', 
-                                   untar=True)
+data_dir = tf.keras.utils.get_file('flower_photos', origin=dataset_url, untar=True)
 data_dir = pathlib.Path(data_dir)
 
-batch_size = 16
+batch_size = 32
 img_height = 180
 img_width = 180
-
 
 train_ds = tf.keras.preprocessing.image_dataset_from_directory(
   data_dir,
@@ -32,31 +32,37 @@ val_ds = tf.keras.preprocessing.image_dataset_from_directory(
   image_size=(img_height, img_width),
   batch_size=batch_size)
 
+num_classes = 5
 
-normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
+data_preprocessing = keras.Sequential([layers.experimental.preprocessing.RandomFlip("horizontal", input_shape=(img_height, img_width, 3)), layers.experimental.preprocessing.RandomRotation(0.1), layers.experimental.preprocessing.RandomZoom(0.1), layers.experimental.preprocessing.Rescaling(1./255, input_shape=(img_height, img_width, 3))])
 
-
-model = tf.keras.Sequential([
-  layers.experimental.preprocessing.Rescaling(1./255),
-  layers.Conv2D(32, 3, input_shape=(img_width, img_height, 3), activation='relu'),
-  layers.MaxPooling2D(2,2),
-  layers.Dropout(0.2),
-  layers.Conv2D(64, 3, activation='relu'),
-  layers.MaxPooling2D(2,2),
-  layers.Dropout(0.3),
-  layers.Flatten(),
-  layers.Dense(64, activation="relu"),
-  layers.Dropout(0.5),
-  layers.Dense(5, activation="softmax")
+model = Sequential([
+  data_preprocessing,
+  Conv2D(16, 3, padding='same', activation='relu'),
+  MaxPooling2D(),
+  Dropout(0.2),
+  Conv2D(32, 3, padding='same', activation='relu'),
+  MaxPooling2D(),
+  Dropout(0.2),
+  Conv2D(64, 3, padding='same', activation='relu'),
+  MaxPooling2D(),
+  Dropout(0.2),
+  Flatten(),
+  Dense(128, activation='relu'),
+  Dense(num_classes)
 ])
 
-model.compile(
-  optimizer=tf.keras.optimizers.RMSprop(learning_rate=0.001, decay=1e-06),
-  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True),
-  metrics=['accuracy'])
 
-model.fit(
+model.compile(optimizer='RMSProp',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+early_stopping = EarlyStopping(monitor='val_accuracy', patience=5, min_delta=0.01, mode='max')
+
+epochs=100
+history = model.fit(
   train_ds,
   validation_data=val_ds,
-  epochs=4
+  epochs=epochs,
+  callbacks=[early_stopping]
 )
